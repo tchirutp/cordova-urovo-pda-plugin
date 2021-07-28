9c66abe5feb09aabd706b2bd107f12ef53759a6b
tchirutp <88089403+tchirutp@users.noreply.github.com>
2021-07-29 00:09:01 +0700

    Update UrovoBroadcastReceiver.java

--- src/android/UrovoBroadcastReceiver.java
+++ src/android/UrovoBroadcastReceiver.java
@@ -5,10 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.os.Vibrator;
 
 public class UrovoBroadcastReceiver extends BroadcastReceiver {
-    private Vibrator vibrator;
     private SoundPool soundpool = null;
     private int soundid;
     private String barcodeStr;
@@ -16,8 +14,6 @@ public class UrovoBroadcastReceiver extends BroadcastReceiver {
     private BarcodeListener listener;
 
     public UrovoBroadcastReceiver(Context context) {
-        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-
         soundpool = new SoundPool(1, AudioManager.STREAM_NOTIFICATION, 100); // MODE_RINGTONE
         soundid = soundpool.load("/etc/Scan_new.ogg", 1);
     }
@@ -26,14 +22,11 @@ public class UrovoBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         isScaning = false;
         soundpool.play(soundid, 1, 1, 0, 0, 1);
-        vibrator.vibrate(100);
 
         byte[] barcode = intent.getByteArrayExtra("barocode");
         int barocodelen = intent.getIntExtra("length", 0);
         byte temp = intent.getByteExtra("barcodeType", (byte) 0);
-        android.util.Log.i("debug", "----codetype--" + temp);
         barcodeStr = new String(barcode, 0, barocodelen);
-        System.out.println("barcode= " + barcodeStr);
         if (listener != null) {
             listener.onBarcodeScanned(barcodeStr);
         }
