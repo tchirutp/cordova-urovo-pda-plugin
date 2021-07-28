90288f14e13dac912bc183cf39b2d5b92f57f56c
tchirutp <88089403+tchirutp@users.noreply.github.com>
2021-07-28 21:01:34 +0700

    Update urovo.js

--- www/urovo.js
+++ www/urovo.js
@@ -1,9 +1,12 @@
 var exec = require('cordova/exec')
+module.exports = {
+  doScan : function(code, success, error) {
+    exec(success, error, 'UrovoPDAPlugin', 'doScan', [code]);
+  },
+  
+  onBarcodeScanned : function(success, error) {
+    exec(success, error, 'UrovoPDAPlugin', 'onBarcodeScanned');
+  }
 
-exports.doScan = function(code, success, error) {
-  exec(success, error, 'UrovoPDAPlugin', 'doScan', [code]);
 }
 
-exports.onBarcodeScanned = function(success, error) {
-  exec(success, error, 'UrovoPDAPlugin', 'onBarcodeScanned');
-}
